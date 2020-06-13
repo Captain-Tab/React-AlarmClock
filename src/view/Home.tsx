828bcd65c5bbea31c77ce376af31218c024e71bb
Tab <d491405879@gmail.com>
2020-06-14 07:23:40 +0800

    添加页脚footer

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -4,20 +4,21 @@ import {DownOutlined} from '@ant-design/icons';
 import {LogoutOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons/lib';
 import history from '../http/history';
 import axios from '../http/axios';
-import Todo from '../component/todo/Todo'
+import Todo from '../component/todo/Todo';
 import Tomato from '../component/tomato/Tomato';
 import {connect} from 'react-redux';
 import {initTodo} from '../redux/action/TodoAction';
 import {initTomato} from '../redux/action/TomatoAction';
-import logoImg from '../assets/React-icon.svg'
+import logoImg from '../assets/React-icon.svg';
+import footerImg from '../assets/footer-logo.png'
 import '../style/Home.scss';
 import Statistics from '../component/statistic/Statistics';
 
 
 interface IRouter {
   history: any
-  initTodo:(params: any)=> any
-  initTomato:(params: any)=>any
+  initTodo: (params: any) => any
+  initTomato: (params: any) => any
 }
 
 interface IindexState {
@@ -30,9 +31,9 @@ const logOut = () => {
   message.info('退出成功', 1);
 };
 
-const onWait = () =>{
-  message.info('该功能正在开发',3)
-}
+const onWait = () => {
+  message.info('该功能正在开发', 3);
+};
 
 const menu = (
   <Menu>
@@ -60,7 +61,7 @@ class Home extends React.Component<IRouter, IindexState> {
   async componentDidMount() {
     await this.receiveUserData();
     await this.getTodos();
-    await this.getTomato()
+    await this.getTomato();
   }
 
   login = () => {
@@ -104,15 +105,25 @@ class Home extends React.Component<IRouter, IindexState> {
           </div>
           <Dropdown overlay={menu} trigger={['click']}>
             <Button type="link" className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-              <UserOutlined /> {this.state.user.account}<DownOutlined/>
+              <UserOutlined/> {this.state.user.account}<DownOutlined/>
             </Button>
           </Dropdown>
         </header>
+
         <main>
           <Tomato/>
           <Todo/>
         </main>
-        <Statistics />
+
+        <Statistics/>
+
+        <footer>
+          <div className="sourceCode"><a href="https://github.com/Captain-Tab/React-AlarmClock">源码地址</a></div>
+          <div className="mainTools">
+            <span>主要技术</span>
+            <img src={footerImg} alt="footer-logo"/>
+          </div>
+        </footer>
       </div>
     );
   }
